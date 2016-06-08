93e2fab2099a0437c2d91c9a3cca7b61fc74389b
komike <kompike@mail.ru>
2016-06-09 02:21:56 +0300

    Tests added

---
+++ src/test/java/com/teamdev/javaclasses/brainfuck/BrainfuckInterpreterTest.java
@@ -0,0 +1,38 @@
+package com.teamdev.javaclasses.brainfuck;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BrainfuckInterpreterTest {
+
+    private Interpreter interpreter = new BrainfuckInterpreter();
+    private String helloWorld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---" +
+            ".+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals("Obtained value doesn\'t equals \'Hello world!\'", "Hello world!", interpreter.interpret(helloWorld));
+    }
+
+    @Test
+    public void testPlusCommand() {
+        assertEquals("Obtained value doesn\'t equals \'1\'", "1", interpreter.interpret("+"));
+    }
+
+    @Test
+    public void testMinusCommand() {
+        assertEquals("Obtained value doesn\'t equals \'-1\'", "-1", interpreter.interpret("-"));
+    }
+
+    @Test
+    public void testForwardCommand() {
+        assertEquals("Forward command doesn't execute", "Hello world!", interpreter.interpret(">"));
+    }
+
+    @Test
+    public void testBackCommand() {
+        assertEquals("Back command doesn't execute", "Hello world!", interpreter.interpret("<"));
+    }
+
+}
