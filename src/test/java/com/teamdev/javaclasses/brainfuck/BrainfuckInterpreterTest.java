f969455c5d3c75369adbc7b8586a82bfb1b36c4f
kompike <kompanietskostya@gmail.com>
2016-06-09 11:02:08 +0300

    Tests successfully passed

--- src/test/java/com/teamdev/javaclasses/brainfuck/BrainfuckInterpreterTest.java
+++ src/test/java/com/teamdev/javaclasses/brainfuck/BrainfuckInterpreterTest.java
@@ -7,32 +7,22 @@ import static org.junit.Assert.*;
 public class BrainfuckInterpreterTest {
 
     private Interpreter interpreter = new BrainfuckInterpreter();
-    private String helloWorld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---" +
+    private final String HELLO_WORLD = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---" +
             ".+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
 
     @Test
     public void testHelloWorld() {
-        assertEquals("Obtained value doesn\'t equals \'Hello World!\'", "Hello World!\n", interpreter.interpret(helloWorld));
+        assertEquals("Obtained value doesn\'t equals \'Hello World!\'", "Hello World!\n", interpreter.interpret(HELLO_WORLD));
     }
 
     @Test
-    public void testPlusCommand() {
-        assertEquals("Obtained value doesn\'t equals \'1\'", "1", interpreter.interpret("+"));
+    public void testBrainfuckCommands() {
+        assertEquals("Obtained value doesn\'t equals \'Q\'", "Q", interpreter.interpret("+++[>+++[>+++[>+++<-]<-]<-]>>>."));
     }
 
     @Test
-    public void testMinusCommand() {
-        assertEquals("Obtained value doesn\'t equals \'-1\'", "-1", interpreter.interpret("-"));
-    }
-
-    @Test
-    public void testForwardCommand() {
-        assertEquals("Forward command doesn't execute", ">", interpreter.interpret(">"));
-    }
-
-    @Test
-    public void testBackCommand() {
-        assertEquals("Back command doesn't execute", "<", interpreter.interpret("<"));
+    public void testPrintCommand() {
+        assertEquals("Obtained value's length doesn\'t equals 1", 1, interpreter.interpret("++++++++[>++++++++<-]>+.").length());
     }
 
     @Test(expected = IllegalArgumentException.class)
